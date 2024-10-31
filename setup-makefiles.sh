kartik-commits/platform_device_xiaomi_miuicamera-redwood

Provide a unique name for system version of vendor.xiaomi.hardware.campostproc@1.0
Change-Id: I853a40fb3996db3ee0861eabf7e290461223cc31
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

setup-makefiles.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project#
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -23,6 +22,33 @@ if [ ! -f "${HELPER}" ]; then
     exit 1
 fi
 source "${HELPER}"
+
+function vendor_imports() {
+    cat << EOF >> "$1"
+                "device/xiaomi/miuicamera-cupid",
+EOF
+}
+
+function lib_to_package_fixup_system_variants() {
+    if [ "$2" != "system" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        vendor.xiaomi.hardware.campostproc@1.0)
+            echo "$1-system"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_system_variants "$@"
+}
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"

rexprjkt/android_device_xiaomi_miuicamera-sapphire

Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

setup-makefiles.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project#
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -23,6 +22,33 @@ if [ ! -f "${HELPER}" ]; then
     exit 1
 fi
 source "${HELPER}"
+
+function vendor_imports() {
+    cat << EOF >> "$1"
+                "device/xiaomi/miuicamera-sapphire",
+EOF
+}
+
+function lib_to_package_fixup_system_variants() {
+    if [ "$2" != "system" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        vendor.xiaomi.hardware.campostproc@1.0)
+            echo "$1-system"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_system_variants "$@"
+}
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"